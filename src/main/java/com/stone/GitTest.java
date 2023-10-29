967f464c9be3e511b8f5c662915f390683ee40f1
stones <zhangnathan2@gmail.com>
2023-10-29 19:02:34 +0800

    first commit

---
+++ src/main/java/com/stone/GitTest.java
@@ -0,0 +1,17 @@
+package com.stone;
+
+import java.sql.SQLOutput;
+
+/**
+ * @ClassName:GitTest
+ * @Auther: 石玫瑰
+ * @Description:
+ * @Date: 2023/10/29 18:57
+ * @Version: v1.0
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello,git!");
+    }
+}
